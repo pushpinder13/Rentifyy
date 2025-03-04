4badcdf8090a7b619cdfee5438b3ccd7fcbd2f62
Pushpinder Singh <Pushpinder9040@gmail.com>
2025-03-04 19:28:48 +0530

    backend intilized

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config()
+
+//middleware
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//routes
+app.get('/', (req, res) => {
+    res.send("hlo");
+});
+
+//listen port
+app.listen(process.env.PORT, () => {
+    console.log(`Running on: http://localhost:${process.env.PORT}`);
+});
